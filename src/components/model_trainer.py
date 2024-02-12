f9ffc1d3b7e78bd6598256f09fdc6107f01fbe3e
Windows <vardhinitelu.25@gmail.com>
2024-02-12 10:14:59 +0530

    Flask app

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -11,7 +11,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 #from sklearn.neighbors import KNeighborsRegressor
 #from sklearn.tree import DecisionTreeRegressor
-from xgboost import XGBRegressor
+# from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
@@ -40,7 +40,6 @@ class ModelTrainer:
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
                 "AdaBoost Regressor": AdaBoostRegressor(),
-                "XGBRegressor": XGBRegressor(),
             }
             params={
                 
@@ -59,10 +58,7 @@ class ModelTrainer:
                     'n_estimators': [8,16,32,64,128,256]
                 },    
                 "Linear Regression":{},
-                "XGBRegressor":{
-                    'learning_rate':[.1,.01,.05,.001],
-                    'n_estimators': [8,16,32,64,128,256]
-                },            
+          
                 "AdaBoost Regressor":{
                     'learning_rate':[.1,.01,0.5,.001],
                     # 'loss':['linear','square','exponential'],
